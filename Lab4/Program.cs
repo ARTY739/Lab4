b6d93502cc41dc1e853edb29c70ef4221a663169
ARTY739 <i.arty200@gmail.com>
2024-03-26 15:34:07 +0300

    Update Program.cs

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -4,11 +4,44 @@
     {
         IAccountManager accountManager = new AccountManager();
 
-        accountManager.OpenAccount("123456");
-        accountManager.Deposit("123456", 1000);
-        accountManager.Withdraw("123456", 500);
-        accountManager.CloseAccount("123456");
+        while (true)
+        {
+            Console.WriteLine("Выберите операцию (1 - Открыть счёт, 2 - Положить деньги на счёт, 3 - Снять деньги со счёта, 4 - Закрыть счёт): ");
+            int choice = Convert.ToInt32(Console.ReadLine());
 
-        Console.ReadLine();
+            if (choice == 1)
+            {
+                Console.WriteLine("Выберите номер счёта: ");
+                string number = Console.ReadLine();
+                accountManager.OpenAccount(number);
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Выберите номер счёта: ");
+                string number = Console.ReadLine();
+                Console.WriteLine("Выберите сумму: ");
+                int money = Convert.ToInt32(Console.ReadLine());
+                accountManager.Deposit(number, money);
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Выберите номер счёта: ");
+                string number = Console.ReadLine();
+                Console.WriteLine("Выберите сумму: ");
+                int money = Convert.ToInt32(Console.ReadLine());
+                accountManager.Withdraw(number, money);
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("Выберите номер счёта: ");
+                string number = Console.ReadLine();
+                accountManager.CloseAccount(number);
+            }
+            else
+            {
+                Console.WriteLine("Неверный выбор.");
+                continue;
+            }
+        }
     }
 }
